b8dd0bca6e0289e61f2e5a9b747859be176c29e6
Vivekshrotriya1 <vivekshrotriyas123@gmail.com>
2025-05-31 14:20:07 +0530

    addition

--- devTinder-web-main/src/utils/constants.js
+++ devTinder-web-main/src/utils/constants.js
@@ -1,3 +1,8 @@
+// export const BASE_URL =
+//   location.hostname === "localhost" ? "https://devcircle.onrender.com" : "/api";
+// // export const BASE_URL = "http://localhost:7777";
+
 export const BASE_URL =
-  location.hostname === "localhost" ? "https://devcircle.onrender.com" : "/api";
-// export const BASE_URL = "http://localhost:7777";
+  location.hostname === "localhost"
+    ? "http://localhost:7777"
+    : "https://devcircle.onrender.com";
