2fbba0d8df505570749eb0eaaec66dc9d660018b
Vivekshrotriya1 <vivekshrotriyas123@gmail.com>
2025-05-31 15:02:26 +0530

    chats

--- devTinder-main-backend/src/utils/socket.js
+++ devTinder-main-backend/src/utils/socket.js
@@ -13,7 +13,11 @@ const getSecretRoomId = (userId, targetUserId) => {
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+     origin: [
+        "http://localhost:5173",
+        "https://devcircle-two.vercel.app"
+      ],
+      credentials: true,
     },
   });
 
--- devTinder-web-main/src/utils/socket.js
+++ devTinder-web-main/src/utils/socket.js
@@ -1,10 +1,20 @@
+// import io from "socket.io-client";
+// import { BASE_URL } from "./constants";
+
+// export const createSocketConnection = () => {
+//   if (location.hostname === "localhost") {
+//     return io(BASE_URL);
+//   } else {
+//     return io("/", { path: "/api/socket.io" });
+//   }
+// };
 import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
-    return io(BASE_URL);
+    return io(BASE_URL, { withCredentials: true });
   } else {
-    return io("/", { path: "/api/socket.io" });
+    return io("https://devcircle.onrender.com", { withCredentials: true });
   }
 };
