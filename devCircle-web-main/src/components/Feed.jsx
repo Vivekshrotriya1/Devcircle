970cfb81b5fec833ab8faedd33f5463e176a33bf
Vivekshrotriya1 <vivekshrotriyas123@gmail.com>
2026-04-16 16:49:14 +0530

    add

--- devCircle-web-main/src/components/Feed.jsx
+++ devCircle-web-main/src/components/Feed.jsx
@@ -11,30 +11,36 @@ const Feed = () => {
 
   const getFeed = async () => {
     if (feed) return;
+
     try {
+      const token = localStorage.getItem("token");
+
       const res = await axios.get(BASE_URL + "/feed", {
-        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`, // ✅ FIX
+        },
       });
+
       dispatch(addFeed(res?.data?.data));
     } catch (err) {
-      //TODO: handle error
+      console.error(err);
     }
   };
 
   useEffect(() => {
     getFeed();
   }, []);
+
   if (!feed) return;
 
   if (feed.length <= 0)
-    return <h1 className="flex justify-center my-10">No new users founds!</h1>;
+    return <h1 className="flex justify-center my-10">No new users found!</h1>;
 
   return (
-    feed && (
-      <div className="flex justify-center my-10">
-        <UserCard user={feed[0]} />
-      </div>
-    )
+    <div className="flex justify-center my-10">
+      <UserCard user={feed[0]} />
+    </div>
   );
 };
+
 export default Feed;
